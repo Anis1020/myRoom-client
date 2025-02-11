a056cfb405d30cbe3887baaf01b9a72e99a70f79
nexgensoft0 <nexgensoft0@gmail.com>
2025-02-11 11:28:28 +0600

    login registration and room detail functionality added

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,12 +1,30 @@
-import { initializeApp } from 'firebase/app'
+// import { initializeApp } from 'firebase/app'
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// }
+
+// export const app = initializeApp(firebaseConfig)
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-}
+  apiKey: "AIzaSyBba1koiCWucxaSdPuF6GT57Hf5-plq-0E",
+  authDomain: "room-rent-bcb20.firebaseapp.com",
+  projectId: "room-rent-bcb20",
+  storageBucket: "room-rent-bcb20.firebasestorage.app",
+  messagingSenderId: "885967913034",
+  appId: "1:885967913034:web:5dd31908b1fbd3ba1826b3",
+};
 
-export const app = initializeApp(firebaseConfig)
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
